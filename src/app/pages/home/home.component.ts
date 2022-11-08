2ba728b6b303e8447bd93158ca7ee728023e410e
Loan PIROTAIS <pirotais.loan@gmail.com>
2022-11-08 10:33:33 +0100

    feat(components): handling templating of home page
    - Putting the movie-details information in a modal
    - Displaying now-playing and popular movies lists

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,34 @@
 import { Component } from "@angular/core";
+import { Subscription } from "rxjs";
+import { MovieDetails } from "src/common/schemas/movie-details.schema";
+import { ApiService } from "src/common/services/api.service";
 
 @Component({
     selector: 'app-home',
     styleUrls: ['./home.component.scss'],
     templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public isOpenedModal = false;
+    public details!: MovieDetails;
+
+    private getMovieDetailSubscription!: Subscription;
+
+    constructor(private readonly apiService: ApiService) {}
+
+    public cancelModal(): void {
+        this.isOpenedModal = false;
+    }
+
+    public openModal(id: number): void {
+        this.getMovieDetailSubscription = this.apiService.getDetail(id).subscribe(
+            (result: MovieDetails) => {
+                this.details = result;
+            },
+            () => {
+                this.getMovieDetailSubscription.unsubscribe();
+            }
+        );
+        this.isOpenedModal = true;
+    }
+}
